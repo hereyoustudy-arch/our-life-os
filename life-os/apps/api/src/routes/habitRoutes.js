c1eeae166b45d2f04798920e61e996897415141d
hereyoustudy-arch <hereyoustudy@gmail.com>
2026-06-20 14:34:27 +0500

    Delete life-os directory

--- life-os/apps/api/src/routes/habitRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const habitController = require('../controllers/habitController');
-const router = express.Router();
-router.get('/', habitController.getHabits);
-router.post('/', habitController.createHabit);
-module.exports = router;
--- life-os/apps/api/src/routes/taskRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const taskController = require('../controllers/taskController');
-
-const router = express.Router();
-
-router.get('/', taskController.getTasks);
-router.post('/', taskController.createTask);
-
-module.exports = router;
