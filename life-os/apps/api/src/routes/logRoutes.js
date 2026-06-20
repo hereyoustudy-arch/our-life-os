6736ac76e7a37202dcdbd4c1cd5a50c774ceca39
hereyoustudy-arch <hereyoustudy@gmail.com>
2026-06-20 11:22:35 +0500

    Delete life-os directory

--- life-os/apps/api/src/routes/logRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const logController = require('../controllers/logController');
-const router = express.Router();
-router.get('/', logController.getLogs);
-router.post('/', logController.createLog);
-module.exports = router;
